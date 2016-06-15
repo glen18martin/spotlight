3ab77f3973718578a5f1d5e596299941935f5cd2
glen18martin <glen18martin@users.noreply.github.com>
2016-06-15 16:10:58 +0530

    seriously

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour {
+
+    public GameObject ball;
+    private Vector3 offset;
+
+
+    void Start()
+    {
+        offset = transform.position - ball.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = ball.transform.position + offset;
+
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    private Rigidbody rb;
+    private int points = 0;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveH = Input.GetAxis("Horizontal");
+        float moveV = Input.GetAxis("Vertical");
+
+        if (moveH == 0) moveH = Input.acceleration.x * 2;
+        if (moveV == 0) moveH = Input.acceleration.y * 2;
+
+
+        rb.AddForce(new Vector3(moveH * 5, 0.0f, moveV * 5));
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("bbad"))
+        {
+            other.gameObject.SetActive(false);
+            points++;
+        }
+    }
+
+}
